f1555975189f5aee33f816f403b0328924288f10
Shahriar Shovon <shahriar.shovon8@gmail.com>
2017-10-18 03:07:03 +0600

    2017-10-18 - done for tonight

--- 01.nodejs_basics.js
+++ 01.nodejs_basics.js
@@ -1,6 +1,6 @@
 //// Example 1
-// var str1 = 'hello';
-// var str2 = new String('hello');
+// let str1 = 'hello';
+// let str2 = new String('hello');
 
 // console.log(str1 == str2);
 // console.log(str1 === str2);
@@ -16,7 +16,7 @@
 
 
 // // Example 2
-// var obj1 = {
+// let obj1 = {
 //     firstName: 'Shahriar',
 //     lastName: 'Shovon'
 // };
@@ -37,7 +37,7 @@
 
 
 // // Example 3
-// var obj2 = function() {
+// let obj2 = function() {
 //     this.firstName = 'Shahriar';
 //     this.lastName = 'Shovon';
 
@@ -115,6 +115,126 @@
 // callbacks.forEach(fn => {
 //     console.log(processNum(5, 0, fn));
 // });
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 6 : Array Operations
+// let array1 = [1, 2, 3, 4, 5, 6];
+// //or let array1 = new Array(1, 2, 3, 4, 5, 6)
+// console.log(array1);
+
+// // add a new item at the end
+// array1.push("shovon");
+// console.log(array1);
+
+// // pop removes the item from the end and returns it
+// let lastElement = array1.pop();
+// console.log(array1);
+// console.log(lastElement);
+
+// // insert at the beginning
+// array1.unshift("hello");
+// console.log(array1);
+
+// // removefrom the beginning
+// let firstElement = array1.shift();
+// console.log(array1);
+// console.log(firstElement);
+
+
+// // traverse array values
+// console.log('traversing array:');
+// array1.forEach((value) => {
+//     console.log(`value: ${value}`);
+// });
+
+
+// // traverse array keys and values
+// console.log('traversing array:');
+// array1.forEach((value, key) => {
+//     console.log(`${key}: ${value}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 7: Associative Array
+// let array2 = new Array();
+// array2['firstName'] = 'Shahriar';
+// array2['lastName'] = 'Shovon';
+// array2['country'] = 'Bangladesh';
+// console.log(array2);
+
+
+// // printing all the keys of associative array
+// for(key in array2) {
+//     console.log(key);
+// }
+
+
+// // printing keys and values of associative array
+// for(key in array2) {
+//     console.log(`My ${key} is ${array2[key]}`);
+// }
+
+
+
+
+
+
+
+
+
+
+// // Example 8: Checking for Key Existence on Associative Array
+// let array3 = new Array();
+// array3['firstName'] = 'Shahriar';
+// array3['lastName'] = 'Shovon';
+// array3['country'] = 'Bangladesh';
+// console.log(array3);
+
+
+// // check whether an associative array has specific property
+// array3.hasProperty = function(qProperty) {
+//     for(key in this) {
+//         if(key === qProperty) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
+
+// console.log(array3.hasProperty('firstName'));
+// console.log(array3.hasProperty('first'));
+
+
+
+
 
 
 
