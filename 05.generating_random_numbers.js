2404e6fed9ae56ac814d631e9876adba2f3bfed8
Shahriar Shovon <shahriar.shovon8@gmail.com>
2017-10-23 00:32:32 +0600

    fixed example numbers

--- 05.generating_random_numbers.js
+++ 05.generating_random_numbers.js
@@ -1,7 +1,7 @@
 // NOTE: The numbers are sorted before it's printed, just to see which numbers were generated(the range)
 
 
-// // Example 49 : Generate random number between 0 and 1
+// // Example 47 : Generate random number between 0 and 1
 // let numbers = [];
 // for(let i = 0; i < 10; i++) {
 //     numbers.push(Math.random());
@@ -22,7 +22,7 @@
 
 
 
-// // Example 50: Generating a random number between min and max (float)
+// // Example 48: Generating a random number between min and max (float)
 // let numbers = [];
 
 // let getRandom = (min, max) => {
@@ -49,7 +49,7 @@
 
 
 
-// // Example 51: Generating a random number between min and max-1 (int)
+// // Example 49: Generating a random number between min and max-1 (int)
 // let numbers = [];
 
 // let getRandom = (min, max) => {
@@ -86,7 +86,7 @@
 
 
 
-// // Example 52: Randomness test
+// // Example 50: Randomness test
 // // returns min+1 to max
 // let getRandom = (min, max) => {
 //     return Math.ceil(Math.random() * (max - min)) + min;
@@ -117,7 +117,7 @@
 
 
 
-// // Example 53: Randomness test
+// // Example 51: Randomness test
 // // returns min to max
 // let getRandom = (min, max) => {
 //     return Math.floor(Math.random() * (max + 1 - min)) + min;
