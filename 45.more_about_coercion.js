5323f5c904c0aba2f5a8e3535ab2f87b2d074418
Shahriar Shovon <shahriar.shovon8@gmail.com>
2018-03-09 07:44:26 +0600

    added examples of javascript native types and coercion

--- 45.more_about_coercion.js
+++ 45.more_about_coercion.js
@@ -605,23 +605,769 @@
 
 
 
-// Example 234 - Falsy - Values that coerce to 'false'
-// Values other than these stated here will be coerced to 'true'.
-// Values that are coerced to 'true' are also called Truthy
+// // Example 234 - Falsy - Values that coerce to 'false'
+// // Values other than these stated here will be coerced to 'true'.
+// // Values that are coerced to 'true' are also called Truthy
+//
+// // Falsy
+// console.log("undefined      =   " + Boolean(undefined));
+// console.log("null           =   " + Boolean(null));
+// console.log("false          =   " + Boolean(false));
+// console.log("+0             =   " + Boolean(+0));
+// console.log("-0             =   " + Boolean(-0));
+// console.log("NaN            =   " + Boolean(NaN));
+// console.log('""             =   ' + Boolean(""));
+//
+// // Truthy
+// console.log("[]             =   " + Boolean([]));
+// console.log("{}             =   " + Boolean({}));
+// console.log("function(){}   =   " + Boolean(function() {}));
+// console.log("1              =   " + Boolean(1));
+// console.log('"hello"        =   ' + Boolean("hello"));
 
-// Falsy
-console.log("undefined      =   " + Boolean(undefined));
-console.log("null           =   " + Boolean(null));
-console.log("false          =   " + Boolean(false));
-console.log("+0             =   " + Boolean(+0));
-console.log("-0             =   " + Boolean(-0));
-console.log("NaN            =   " + Boolean(NaN));
-console.log('""             =   ' + Boolean(""));
 
-// Truthy
-console.log("[]             =   " + Boolean([]));
-console.log("{}             =   " + Boolean({}));
-console.log("function(){}   =   " + Boolean(function() {}));
-console.log("1              =   " + Boolean(1));
-console.log('"hello"        =   ' + Boolean("hello"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 235 - Explicit coercion - String <--> Number
+// // Number() and String() functions are used to explicitly coerce between numbers and strings
+// var a = 42;
+// var b = "47";
+//
+// var as = String(a);
+// var bn = Number(b);
+//
+//
+// console.log(a);
+// console.log(as);
+//
+// console.log(b);
+// console.log(bn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 236 - Date coercion
+// var d = new Date();
+// console.log(+d);
+//
+//
+// var timestamp = +new Date();
+// console.log(timestamp);
+//
+// var timestamp2 = +new Date;
+// console.log(timestamp2);
+// console.log();
+//
+//
+//
+//
+// // but preferable method in ES5
+// var t1 = (new Date()).getTime();
+// var t2 = (new Date).getTime();
+// var t3 = new Date().getTime();
+//
+// console.log(t1);
+// console.log(t2);
+// console.log(t3);
+// console.log();
+//
+//
+// // preferable in ES6
+// console.log(Date.now());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 237 - ~ coercion
+// // ~ is a bitwise operator and does 2's complement on 32-bit number
+// // ~ is the same as -(x+1)
+// // It is used to make -1 false and others true
+// // Used to make codes cleaner in functions that return -1 for failure
+// // such as String.indexOf()
+// var str = 'hello world';
+//
+// if(str.indexOf('lo') >= 0) {
+//     console.log('found');
+// }
+//
+// if(str.indexOf('ol') === -1) {
+//     console.log('not found');
+// }
+//
+// console.log();
+//
+//
+//
+// // with ~
+// console.log(~str.indexOf('lo'));
+// console.log(~str.indexOf('ol'));
+// console.log();
+//
+//
+// // ~ converted code
+// if(~str.indexOf('lo')) {
+//     console.log('found');
+// }
+//
+// if(!~str.indexOf('ol')) {
+//     console.log('not found');
+// }
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 238 - parsing numeric part from a string
+// var borderWidth = "20px";
+// var boxWidth = "34.6%";
+//
+//
+// console.log(parseInt(borderWidth));
+// console.log(parseFloat(boxWidth));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 239 - parseInt can parse hex strings to numbers
+// var colorHex = 'fff is white';
+// console.log(parseInt(colorHex, 16));
+// console.log(parseInt(colorHex, 16).toString(16));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 240 - parsing octal strings to number
+// var oct = "427";
+// console.log(parseInt(oct, 8));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 241 - parsing binary string to number
+// var bin = "1011 is a binary number";
+// console.log(parseInt(bin, 2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 242 - The difference between variable + "" and String(variable)
+// // variable + "" -> calls valueOf()
+// //  String(a) -> calls toString()
+// var a = {
+//     valueOf: function() {
+//         return "Node.js";
+//     },
+//     toString: function() {
+//         return "JavaScript";
+//     }
+// };
+//
+//
+// console.log(a + ""); // Node.js
+// console.log(String(a)); // JavaScript
+//
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 243 - String to Number implicit coercion
+// var a = "3.14";
+//
+// // implicit String -> Number coercion
+// var b = a - 0;
+// var c = a / 1;
+// var d = a * 1;
+//
+// console.log(typeof a, a);
+//
+// console.log(typeof b, b);
+// console.log(typeof c, c);
+// console.log(typeof d, d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 244 - Implicit Boolean coercion
+// // ! coerce to Boolean but it flips the value from Truthy to Falsy
+// // That's why we have to double negate it !!
+// console.log(!!null);
+// console.log(!!undefined);
+// console.log(!!"");
+// console.log(!!false);
+// console.log(!!0);
+// console.log(!!-0);
+// console.log(!!+0);
+// console.log();
+//
+// console.log(!!43);
+// console.log(!!"hello");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 245 - Making sure only one of the argument is Truthy and
+// // the others are Falsy
+// // Works only on Boolean inputs
+// function onlyOne() {
+//     var sum = 0;
+//
+//     for(var i = 0; i < arguments.length; i++) {
+//         if(arguments[i]) {
+//             sum += arguments[i];
+//         }
+//     }
+//
+//     return sum == 1;
+// }
+//
+//
+//
+// console.log(onlyOne(true, false, false, false));
+// console.log(onlyOne(false, false, true, false, false, true));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 245 - IMPROVED - Making sure only one of the argument is Truthy and
+// // the others are Falsy
+// // Works on any type of input
+// function onlyOne() {
+//     var sum = 0;
+//
+//     for(var i = 0; i < arguments.length; i++) {
+//         sum += Number(!!arguments[i]);
+//     }
+//
+//     return sum == 1;
+// }
+//
+//
+//
+// console.log(onlyOne(NaN, undefined, "", null));
+// console.log(onlyOne(NaN, 1, "", false));
+// console.log(onlyOne(NaN, undefined, 1, "", true));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 246 - JavaScript || selector
+// var a = "hello";
+// var b = "world";
+// var c = false;
+//
+//
+// console.log(a || c); // 'hello'
+// console.log(c || b); // 'world'
+//
+// // if both are true, for || selector, the first one is returned
+// // as it stops checking as soon as a Truthy is found
+// console.log(a || b); // 'hello'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 247 - JavaScript && selector
+// var a = "hello";
+// var b = "world";
+// var c = undefined;
+//
+//
+// // returns the value that is coerced to false
+// console.log(a && c); // undefined
+// console.log(c && b); // undefined
+//
+// // if both are Truthy, returns RHS
+// console.log(a && b); // 'world'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 248 - || selector equivalent ternary form
+// // a || b -> a ? a : b
+// (() => {
+//     console.log('a="hello", b=undefined');
+//
+//     var a = 'hello';
+//     var b = undefined;
+//
+//     console.log(a || b);
+//     console.log(a ? a : b);
+// })();
+//
+//
+//
+// console.log();
+// (() => {
+//     console.log('a=undefined, b="world"');
+//
+//     var a = undefined;
+//     var b = 'world';
+//
+//     console.log(a || b);
+//     console.log(a ? a : b);
+// })();
+//
+//
+//
+// console.log();
+// (() => {
+//     console.log('a=undefined, b=null');
+//
+//     var a = undefined;
+//     var b = null;
+//
+//     console.log(a || b);
+//     console.log(a ? a : b);
+// })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 249 - && selector equivalent ternary form
+// // a && b -> a ? b : a
+// (() => {
+//     console.log('a="hello", b=undefined');
+//
+//     var a = 'hello';
+//     var b = undefined;
+//
+//     console.log(a && b);
+//     console.log(a ? b : a);
+// })();
+//
+//
+//
+// console.log();
+// (() => {
+//     console.log('a=undefined, b="world"');
+//
+//     var a = undefined;
+//     var b = 'world';
+//
+//     console.log(a && b);
+//     console.log(a ? b : a);
+// })();
+//
+//
+//
+// console.log();
+// (() => {
+//     console.log('a=undefined, b=null');
+//
+//     var a = undefined;
+//     var b = null;
+//
+//     console.log(a && b);
+//     console.log(a ? b : a);
+// })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 250 - || selector is used to set default values in function
+// function greet(name, msg) {
+//     var name = name || "Shovon";
+//     var msg = msg || "How are you?";
+//
+//     console.log(name + "! " + msg);
+// }
+//
+//
+// greet();
+// greet("Sharmin", "Good Morning");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 251 - && selector is used to check LHS and execute RHS only
+// // if LHS is true
+// function greet(msg) {
+//     msg = msg || "Hello World";
+//     console.log(msg);
+// }
+//
+// var msg = "Good Morning";
+// msg && greet(msg);
+//
+//
+//
+// // greet() function won't execute as msg is Falsy
+// msg = undefined;
+// msg && greet(msg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 252 - null and undefined == comparison
+// // null and undefined are equal to itself and each other
+// // they are not equal to anything else in JS
+// console.log('null == null ->', null == null);
+// console.log('undefined == undefined ->', undefined == undefined);
+// console.log('null == undefined ->', null == undefined);
+// console.log('null == false ->', null == false);
+// console.log('undefined == false ->', undefined == false);
+// console.log('null == 0 ->', null == 0);
+// console.log('undefined == 0 ->', undefined == 0);
+// console.log('null == "" ->', null == "");
+// console.log('undefined == "" ->', undefined == "");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 253 - Comparing objects to non-objects
+// var a = "hello";
+// var b = Object(a);
+//
+// console.log(a === b); // false
+// console.log(a == b); // true
+//
+//
+//
+//
+//
+//
+// // null and undefined can't be boxed
+// var c = null;
+// var d = Object(a); // can't be boxed -> equivalent to Object()
+// console.log(c == d); // false
+//
+//
+// var e = undefined;
+// var f = Object(e); // can't be boxed -> equivalent to Object()
+// console.log(e == f); // false
+//
+//
+//
+//
+// var e = NaN;
+// var f = Object(e); // boxed to Number
+// console.log(e == f); // false as NaN is never equal to itself
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example 254 - Evil tricks from YDKJS - Just for Fun
+// var i = 2;
+//
+// // this is not such a good idea
+// Number.prototype.valueOf = function() {
+//     return i++;
+// };
+//
+// var a = new Number(42);
+//
+// if(a == 2 && a == 3) {
+//     console.log('this must be a bug');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
